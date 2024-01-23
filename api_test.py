1a30a1705382d8e000e5b8305453feba7d83f0c8
ek1erik <eklerik.f@gmail.com>
2024-01-23 15:49:25 +0300

    add api_test.py

---
+++ api_test.py
@@ -0,0 +1,26 @@
+{
+  "document": {
+    "client": "1480369995942764573",
+    "documentProducts": [
+      "6901924475947515907", "690192447594758765"
+    ],
+    "appliesOnAllProducts": false,
+    "documentPersons": [
+      {
+        "party": "1480370292295508038",
+        "authorities": [
+          {
+            "authorityCode": "LOCATION_MONEY_DEP_NSO_LIMITSUM",
+
+          }
+        ]
+      }
+
+request ["document"] = {}
+request ["document"]["client"] = "1480369995942764573"
+request ["document"]["appliesOnAllProducts"] = False
+request ["document"]["documentProducts"] = ["6901924475947515907", "690192447594758765"]
+request ["document"]["documentPersons"] = [{}]
+request ["document"]["documentPersons"][0]["party"] = {"1480370292295508038"}
+request ["document"]["documentPersons"][0]["authorities"] = [{}]
+request ["document"]["documentPersons"][0]["authorities"][0]["authorityCode"] = {"LOCATION_MONEY_DEP_NSO_LIMITSUM"}
